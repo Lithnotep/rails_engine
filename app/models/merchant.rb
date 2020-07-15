5578d7bc8c81f87ca50ddc98ff2acadbcdea1649
Max Mitrani <mjmitrani168@gmail.com>
2020-07-15 17:31:22 -0600

    Add /find and /find_all functionality for merchants

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,11 +5,16 @@ class Merchant < ApplicationRecord
 
   def self.locate_one(params)
     if params[:name]
-      Merchant.where('lower(name) LIKE ?', "%#{params[:name].downcase}%").limit(1)
+      Merchant.where('name ILIKE ?', "%#{params[:name]}%").first
     elsif params[:created_at]
-      Merchant.where('created_at LIKE ?', "%#{params[:created_at]}%").limit(1)
+
+      Merchant.where("merchants.created_at, LIKE ?", "%#{params[:created_at]}%").limit(1)
     elsif params[:updated_at]
-      Merchant.where('updated_at LIKE ?', "%#{params[:updated_at]}%").limit(1)
+      Merchant.where('updated_at LIKE ?', "%#{params[:updated_at].strftime("%Y-%m-%d")}%").limit(1)
     end
+  end
+
+  def self.locate_all(params)
+    Merchant.where('name ILIKE ?', "%#{params[:name]}%")
   end
 end
--- spec/requests/merchant_find_spec.rb
+++ spec/requests/merchant_find_spec.rb
@@ -11,4 +11,33 @@ describe "Merchant find API" do
     expect(parsed["data"].count).to eq(1)
     expect(parsed["data"][0]["id"].to_i).to eq(merchant.id)
   end
+  xit "can get single Merchant via find with created at" do
+    merchant = create(:merchant, name: "Billy Booo", created_at: 'Mon, 16 May 2013 06:30:21 UTC +00:00')
+    merchant2 = create(:merchant, name: "illy Nono")
+    create_list(:merchant, 3)
+    get "/api/v1/merchants/find?created_at=2013"
+    expect(response).to be_successful
+    parsed = JSON.parse(response.body)
+    expect(parsed["data"].count).to eq(1)
+    expect(parsed["data"][0]["id"].to_i).to eq(merchant.id)
+  end
+  xit "can get single Merchant via find with updated at" do
+    merchant = create(:merchant, name: "Billy Booo", updated_at: 'Mon, 16 May 2013 06:30:21 UTC +00:00')
+    merchant2 = create(:merchant, name: "illy Nono")
+    create_list(:merchant, 3)
+    get "/api/v1/merchants/find?updated_at=2013"
+    expect(response).to be_successful
+    parsed = JSON.parse(response.body)
+    expect(parsed["data"].count).to eq(1)
+    expect(parsed["data"][0]["id"].to_i).to eq(merchant.id)
+  end
+  it "can get single Merchant via find all" do
+    merchant = create(:merchant, name: "Billy Booo")
+    merchant2 = create(:merchant, name: "illy Nono")
+
+    get "/api/v1/merchants/find_all?name=lly"
+    expect(response).to be_successful
+    parsed = JSON.parse(response.body)
+    expect(parsed["data"].count).to eq(2)
+  end
 end
